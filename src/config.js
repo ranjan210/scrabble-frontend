cc4d459d8a91a8b8b4b2ad37e0ae4ede8cd81fcc
Ranjan <mynamerkt3@gmail.com>
2023-02-11 18:10:38 +0530

    added new back-end url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_URL : "https://young-gorge-62656.herokuapp.com",
-    IO_URL : "https://young-gorge-62656.herokuapp.com"
+    API_URL : "https://scrabble-backend.onrender.com/",
+    IO_URL : "https://scrabble-backend.onrender.com/"
 }
